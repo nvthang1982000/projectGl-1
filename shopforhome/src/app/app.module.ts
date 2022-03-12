4b451af3a0f804d08546922949912158224c9780
madhuri <madhurisagala@gmail.com>
2022-03-12 08:59:58 +0530

    Angular  components

--- shopforhome/src/app/app.module.ts
+++ shopforhome/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminHomepageComponent } from './admin-homepage/admin-homepage.component';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
+import { OrdersComponent } from './ecommerce/orders/orders.component';
+import { ProductsComponent } from './ecommerce/products/products.component';
+import { ProductsshoppingCartComponent } from './ecommerce/productsshopping-cart/productsshopping-cart.component';
+import { ShoppingCartComponent } from './ecommerce/shopping-cart/shopping-cart.component';
+import { AdminOperationComponent } from './admin-operation/admin-operation.component';
+import { SearchComponent } from './search/search.component';
+import { StockReportComponent } from './stock-report/stock-report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminHomepageComponent,
+    EcommerceComponent,
+    OrdersComponent,
+    ProductsComponent,
+    ProductsshoppingCartComponent,
+    ShoppingCartComponent,
+    AdminOperationComponent,
+    SearchComponent,
+    StockReportComponent
   ],
   imports: [
     BrowserModule,
