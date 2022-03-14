fbb55e327de1b7dc723f40d2d7a0ea6450b8f69d
21. Vũ Văn Tú <tuhdqk@gmail.com>
2022-03-14 16:26:01 +0700

    new update

--- query.sql
+++ query.sql
@@ -1,159 +1,337 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.7.33 - MySQL Community Server (GPL)
--- Server OS:                    Win64
--- HeidiSQL Version:             11.2.0.6213
--- --------------------------------------------------------
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: project1
+-- ------------------------------------------------------
+-- Server version	8.0.22
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `admin`
+--
 
--- Dumping database structure for project1
-DROP DATABASE IF EXISTS `project1`;
-CREATE DATABASE IF NOT EXISTS `project1` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `project1`;
-
--- Dumping structure for table project1.admin
 DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin` (
-  `ID_ad` int(11) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `ID_ad` int NOT NULL AUTO_INCREMENT,
   `Ad_name` varchar(45) NOT NULL,
   `Ad_email` varchar(45) NOT NULL,
   `Ad_UN` varchar(45) NOT NULL,
-  `Ad_PW` varchar(45) NOT NULL,
+  `Ad_PW` varchar(500) NOT NULL,
+  `Ad_image` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`ID_ad`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Dumping data for table `admin`
+--
 
--- Dumping structure for table project1.bill
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bill`
+--
+
 DROP TABLE IF EXISTS `bill`;
-CREATE TABLE IF NOT EXISTS `bill` (
-  `Bill_ID` int(11) NOT NULL,
-  `ProductID_pro` int(11) NOT NULL,
-  `Quantity` int(11) NOT NULL,
-  `UserID_user` int(11) NOT NULL,
-  `PriceProductID_pro` int(11) NOT NULL,
-  `PriceID_Price` int(11) NOT NULL,
-  PRIMARY KEY (`Bill_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bill` (
+  `Bill_ID` int NOT NULL AUTO_INCREMENT,
+  `ProductID_pro` int NOT NULL,
+  `Quantity` int NOT NULL,
+  `UserID_user` int NOT NULL,
+  `PriceID_Price` int NOT NULL,
+  PRIMARY KEY (`Bill_ID`),
+  KEY `ProductID_pro_idx` (`ProductID_pro`),
+  KEY `UserID_user_idx` (`UserID_user`),
+  KEY `PriceID_Price_idx` (`PriceID_Price`),
+  CONSTRAINT `PriceID_Price` FOREIGN KEY (`PriceID_Price`) REFERENCES `price` (`ID_Price`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `ProductID_pro3` FOREIGN KEY (`ProductID_pro`) REFERENCES `product` (`ID_pro`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `UserID_user` FOREIGN KEY (`UserID_user`) REFERENCES `user` (`ID_user`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Dumping data for table `bill`
+--
 
--- Dumping structure for table project1.categories
+LOCK TABLES `bill` WRITE;
+/*!40000 ALTER TABLE `bill` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bill` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
 DROP TABLE IF EXISTS `categories`;
-CREATE TABLE IF NOT EXISTS `categories` (
-  `ID_cat` int(11) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `ID_cat` int NOT NULL AUTO_INCREMENT,
   `Group` varchar(45) NOT NULL,
   PRIMARY KEY (`ID_cat`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Dumping data for table `categories`
+--
 
--- Dumping structure for table project1.coupon
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `coupon`
+--
+
 DROP TABLE IF EXISTS `coupon`;
-CREATE TABLE IF NOT EXISTS `coupon` (
-  `ID_coupon` int(11) NOT NULL,
-  `Discount` int(11) NOT NULL DEFAULT '0',
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `coupon` (
+  `ID_coupon` int NOT NULL AUTO_INCREMENT,
+  `Discount` varchar(45) NOT NULL,
   PRIMARY KEY (`ID_coupon`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Dumping data for table `coupon`
+--
 
--- Dumping structure for table project1.image
+LOCK TABLES `coupon` WRITE;
+/*!40000 ALTER TABLE `coupon` DISABLE KEYS */;
+/*!40000 ALTER TABLE `coupon` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `image`
+--
+
 DROP TABLE IF EXISTS `image`;
-CREATE TABLE IF NOT EXISTS `image` (
-  `ID` int(11) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `image` (
+  `ID` int NOT NULL AUTO_INCREMENT,
   `Path` varchar(45) NOT NULL,
-  `ProductID_pro` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `ProductID_pro` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ProductID_pro_idx` (`ProductID_pro`),
+  CONSTRAINT `ProductID_pro4` FOREIGN KEY (`ProductID_pro`) REFERENCES `product` (`ID_pro`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Dumping data for table `image`
+--
 
--- Dumping structure for table project1.price
+LOCK TABLES `image` WRITE;
+/*!40000 ALTER TABLE `image` DISABLE KEYS */;
+INSERT INTO `image` VALUES (1,'anhdepvl.png',1),(2,'anhdepthatday.png',2),(3,'eooidepvc.png',1),(4,'hihi.png',1),(5,'heheh.jpg',2);
+/*!40000 ALTER TABLE `image` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `price`
+--
+
 DROP TABLE IF EXISTS `price`;
-CREATE TABLE IF NOT EXISTS `price` (
-  `ID_Price` int(11) NOT NULL,
-  `ProductID_pro` int(11) NOT NULL,
-  `Price_bf_dis` int(11) NOT NULL,
-  `Price_af_dis` int(11) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `price` (
+  `ID_Price` int NOT NULL AUTO_INCREMENT,
+  `ProductID_pro` int NOT NULL,
+  `Price_bf_dis` int NOT NULL,
+  `Price_af_dis` int NOT NULL,
   `Start_date` varchar(45) NOT NULL,
-  `CouponID_Coupon` int(11) NOT NULL,
+  `CouponID_Coupon` int NOT NULL,
   `End_Date` varchar(45) NOT NULL,
-  PRIMARY KEY (`ID_Price`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`ID_Price`),
+  KEY `ProductID_pro_idx` (`ProductID_pro`),
+  KEY `CouponID_Coupon_idx` (`CouponID_Coupon`),
+  CONSTRAINT `ProductID_pro5` FOREIGN KEY (`ProductID_pro`) REFERENCES `product` (`ID_pro`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Dumping data for table `price`
+--
 
--- Dumping structure for table project1.product
+LOCK TABLES `price` WRITE;
+/*!40000 ALTER TABLE `price` DISABLE KEYS */;
+INSERT INTO `price` VALUES (1,1,10000,9999,'3/14/2022',1,'3/15/2022'),(2,2,100000,99999,'3/14/2022',2,'3/15/2022');
+/*!40000 ALTER TABLE `price` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
 DROP TABLE IF EXISTS `product`;
-CREATE TABLE IF NOT EXISTS `product` (
-  `ID_pro` int(11) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `ID_pro` int NOT NULL AUTO_INCREMENT,
   `Pro_name` varchar(200) NOT NULL,
-  `Left_Quantity` int(11) NOT NULL,
-  `Price` int(11) NOT NULL,
-  `Flag` varchar(45) NOT NULL,
-  `SupplierID_Supplier` int(11) NOT NULL,
+  `Left_Quantity` int NOT NULL,
+  `Price` int NOT NULL,
+  `Flag_0_1` varchar(45) DEFAULT NULL,
+  `SupplierID_Supplier` int NOT NULL,
   PRIMARY KEY (`ID_pro`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Dumping data for table `product`
+--
 
--- Dumping structure for table project1.product_categories
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'Ghế nhựa',1000,10000,'',1),(2,'Bàn gỗ',10,1000000,NULL,1),(3,'Đèn treo tường',1000,100000,NULL,1),(4,'Quạt trần',100,10000,NULL,1);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_categories`
+--
+
 DROP TABLE IF EXISTS `product_categories`;
-CREATE TABLE IF NOT EXISTS `product_categories` (
-  `id` int(11) NOT NULL,
-  `ProductID_pro` int(11) NOT NULL,
-  `CategoriesID_Cat` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ProductID_pro` int NOT NULL,
+  `CategoriesID_Cat` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ProductID_pro_idx` (`ProductID_pro`),
+  KEY `CategoriesID_Cat_idx` (`CategoriesID_Cat`),
+  CONSTRAINT `CategoriesID_Cat` FOREIGN KEY (`CategoriesID_Cat`) REFERENCES `categories` (`ID_cat`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `ProductID_pro1` FOREIGN KEY (`ProductID_pro`) REFERENCES `product` (`ID_pro`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Dumping data for table `product_categories`
+--
 
--- Dumping structure for table project1.supplier
+LOCK TABLES `product_categories` WRITE;
+/*!40000 ALTER TABLE `product_categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `supplier`
+--
+
 DROP TABLE IF EXISTS `supplier`;
-CREATE TABLE IF NOT EXISTS `supplier` (
-  `ID_Supplier` int(11) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `supplier` (
+  `ID_Supplier` int NOT NULL AUTO_INCREMENT,
   `Sup_name` varchar(100) NOT NULL,
   `Sup_email` varchar(50) NOT NULL,
   `Sup_hotline` varchar(50) NOT NULL,
   PRIMARY KEY (`ID_Supplier`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Dumping data for table `supplier`
+--
 
--- Dumping structure for table project1.user
+LOCK TABLES `supplier` WRITE;
+/*!40000 ALTER TABLE `supplier` DISABLE KEYS */;
+/*!40000 ALTER TABLE `supplier` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `ID_user` int(11) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `ID_user` int NOT NULL AUTO_INCREMENT,
   `User_name` varchar(200) NOT NULL,
   `User_address` varchar(200) NOT NULL,
   `User_email` varchar(50) NOT NULL,
   `User_phone` varchar(45) NOT NULL,
   `User_UN` varchar(45) NOT NULL,
-  `User_PW` varchar(45) NOT NULL,
+  `User_PW` varchar(500) NOT NULL,
+  `User_image` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`ID_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Dumping data for table `user`
+--
 
--- Dumping structure for table project1.wishlist
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `wishlist`
+--
+
 DROP TABLE IF EXISTS `wishlist`;
-CREATE TABLE IF NOT EXISTS `wishlist` (
-  `id` int(11) NOT NULL,
-  `ProductID_pro` int(11) NOT NULL,
-  `idUser` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `wishlist` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ProductID_pro` int NOT NULL,
+  `idUser` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idUser_idx` (`idUser`),
+  KEY `ProductID_pro_idx` (`ProductID_pro`),
+  CONSTRAINT `idUser` FOREIGN KEY (`idUser`) REFERENCES `user` (`ID_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `ProductID_pro` FOREIGN KEY (`ProductID_pro`) REFERENCES `product` (`ID_pro`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Dumping data for table `wishlist`
+--
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+LOCK TABLES `wishlist` WRITE;
+/*!40000 ALTER TABLE `wishlist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `wishlist` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'project1'
+--
+
+--
+-- Dumping routines for database 'project1'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-14 15:52:11
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 #Project database diagrams<br />
-![alt text](https://raw.githubusercontent.com/alta1212/projectGl/main/img/diagrams.png)<br />
+![alt text](https://raw.githubusercontent.com/alta1212/projectGl/main/img/dbproject1.png)<br />
 #For swagger visit /swagger-ui.html
