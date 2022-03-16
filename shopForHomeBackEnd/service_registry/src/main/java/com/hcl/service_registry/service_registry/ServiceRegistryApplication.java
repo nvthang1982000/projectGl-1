2dab089236cde3bed65f49d6a9c7adcc21391205
alta1212 <doremon209a@gmail.com>
2022-03-17 01:23:25 +0700

    đ

--- shopForHomeBackEnd/service_registry/src/main/java/com/hcl/service_registry/service_registry/ServiceRegistryApplication.java
+++ shopForHomeBackEnd/service_registry/src/main/java/com/hcl/service_registry/service_registry/ServiceRegistryApplication.java
@@ -1,7 +1,14 @@
 package com.hcl.service_registry.service_registry;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication 
 @EnableEurekaServer
@@ -9,6 +16,16 @@ public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("http://localhost:4200")
+				.allowedMethods("POST","GET","DELETE","PUT");
+			}
+		};
 	}
 
 }
